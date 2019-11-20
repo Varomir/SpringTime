7e7bc6921735508aa14887552bc369886b24684e
Vadym Feshchenko <VFeshchenko@ten-x.com>
2019-11-20 15:36:02 +0200

    [Step07] - Extend previous 'DI' example with Spring Properties

--- src/main/java/com/varomir/spring/MusicPlayer.java
+++ src/main/java/com/varomir/spring/MusicPlayer.java
@@ -3,7 +3,14 @@ package com.varomir.spring;
 public class MusicPlayer {
     private Music music;
 
+    public MusicPlayer() {
+    }
+
     public MusicPlayer(Music music) {
+        this.music = music;
+    }
+
+    public void setMusic(Music music) {
         this.music = music;
     }
 
