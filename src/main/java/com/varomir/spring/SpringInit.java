c78d804dfbcd487c6618215840a7d1c63fc5f452
Vadym Feshchenko <VFeshchenko@ten-x.com>
2019-11-21 17:54:14 +0200

    [Step09] - Add example with 'init-method' and 'prototype' scope

--- src/main/java/com/varomir/spring/SpringInit.java
+++ src/main/java/com/varomir/spring/SpringInit.java
@@ -6,6 +6,14 @@ public class SpringInit {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("ApplicationContext.xml");
 
+
+        Music pop1  = ctx.getBean("pop", PopMusic.class);
+        Music pop2  = ctx.getBean("pop", PopMusic.class);
+        Music pop3  = ctx.getBean("pop", PopMusic.class);
+        System.out.println(pop1.getSong());
+        System.out.println(pop2.getSong());
+        System.out.println(pop3.getSong());
+
         MusicPlayer jbl  = ctx.getBean("jbl", MusicPlayer.class);
         jbl.playMusic();
 
