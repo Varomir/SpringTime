d9a2319dd97bc6543eb7a760e8af12f0073ca016
Vadym Feshchenko <VFeshchenko@ten-x.com>
2019-11-21 22:56:30 +0200

    [Step10] - Add MusicPlayer with simple IoC demo

--- src/main/java/com/varomir/spring/SpringInit.java
+++ src/main/java/com/varomir/spring/SpringInit.java
@@ -2,14 +2,17 @@ package com.varomir.spring;
 
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import java.util.Arrays;
+
 public class SpringInit {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("ApplicationContext.xml");
 
-        Music alternative  = ctx.getBean("alternativeMusic", AlternativeMusic.class);
-        Music rockNRoll  = ctx.getBean("rockNRollMusic", RockNRollMusic.class);
-        System.out.println("Playing alternative music: " + alternative.getSong());
-        System.out.println("Playing rock'n'roll music: " + rockNRoll.getSong());
+        Music alternative = ctx.getBean("alternativeMusic", AlternativeMusic.class);
+        Music rockNRoll = ctx.getBean("rockNRollMusic", RockNRollMusic.class);
+
+        MusicPlayer iPod = new MusicPlayer(Arrays.asList(alternative, rockNRoll));
+        iPod.playMusic();
 
         ctx.close();
     }
