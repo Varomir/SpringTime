3f659dbe4e3874f8e88b1212aacf30f78828f78c
Vadym Feshchenko <VFeshchenko@ten-x.com>
2019-11-21 16:22:55 +0200

    [Step09] - Preparation for demonstrate Spring 'init/destroy-method' specific functionalities

--- src/main/java/com/varomir/spring/PopMusic.java
+++ src/main/java/com/varomir/spring/PopMusic.java
@@ -6,4 +6,12 @@ public class PopMusic implements Music {
     public String getSong() {
         return "Abba - Money, Money, Money";
     }
+
+    public void initBean() {
+        System.out.println(">>  Doing some specific PopMusic stuff initialization. Playing 15-seconds advertisements.");
+    }
+
+    public void destroyBean() {
+        System.out.println(">>  Doing some specific PopMusic stuff destruction. Playing a notification message with the current time.");
+    }
 }
