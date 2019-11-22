249147852af185fe5faccf464740a15d05fa849d
Vadym Feshchenko <VFeshchenko@ten-x.com>
2019-11-22 12:18:57 +0200

    [Step11] - Fix: remove unused variable

--- src/main/java/com/varomir/spring/SpringInit.java
+++ src/main/java/com/varomir/spring/SpringInit.java
@@ -2,17 +2,11 @@ package com.varomir.spring;
 
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-import java.util.Arrays;
-
 public class SpringInit {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("ApplicationContext.xml");
 
-        Music alternative = ctx.getBean("alternativeMusic", AlternativeMusic.class);
-        Music rockNRoll = ctx.getBean("rockNRollMusic", RockNRollMusic.class);
-
         MusicPlayer jbl = ctx.getBean("musicPlayer", MusicPlayer.class);
-
         jbl.playMusic();
 
         ctx.close();
