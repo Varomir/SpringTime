7a5e64898b6bc89675420d41d3b642bda967c117
Vadym Feshchenko <VFeshchenko@ten-x.com>
2019-11-22 16:03:46 +0200

    [Step12] - Update the 'Music' interface child classes, to satisfy new signature

--- src/main/java/com/varomir/spring/AlternativeMusic.java
+++ src/main/java/com/varomir/spring/AlternativeMusic.java
@@ -2,11 +2,14 @@ package com.varomir.spring;
 
 import org.springframework.stereotype.Component;
 
+import java.util.Arrays;
+import java.util.List;
+
 @Component
 public class AlternativeMusic implements Music {
     
     @Override
-    public String getSong() {
-        return "Korn - Did My Time";
+    public List<String> getSong() {
+        return Arrays.asList("Korn - Did My Time", "System Of A Down - Chop Suey!", "Limp Bizkit - Nookie");
     }
 }
