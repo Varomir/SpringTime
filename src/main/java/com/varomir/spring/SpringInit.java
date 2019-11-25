cc4cf843fc2ffc5f106ea9e9d6bc075d5c1385fa
Vadym Feshchenko <VFeshchenko@ten-x.com>
2019-11-25 14:57:10 +0200

    [Step13] - Modify example with 'PopMusic' 'prototype' Scope annotation

--- src/main/java/com/varomir/spring/SpringInit.java
+++ src/main/java/com/varomir/spring/SpringInit.java
@@ -6,10 +6,10 @@ public class SpringInit {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("ApplicationContext.xml");
 
-        MusicPlayer radioPlayer = ctx.getBean("musicPlayer", MusicPlayer.class);
-        radioPlayer.playMusic(MusicType.POP);
-        System.out.println("Player brand: " + radioPlayer.getName());
-        System.out.println("Player volume: " + radioPlayer.getVolume());
+        Music pop1 = ctx.getBean("popMusic", PopMusic.class);
+        Music pop2 = ctx.getBean("popMusic", PopMusic.class);
+        System.out.println("Are 2 beans are the same: " + (pop1 == pop2));
+        System.out.println("pop1.hash=" + pop1.hashCode() + " VS pop2.hash=" + pop2.hashCode());
 
         ctx.close();
     }
